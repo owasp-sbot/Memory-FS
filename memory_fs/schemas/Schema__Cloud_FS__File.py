8dbda3bfe7fb0bfd8136394d8cdcddf5cb3e1636
Dinis Cruz <dinis.cruz@owasp.org>
2025-05-27 10:51:37 +0100

    refactored in cloud_fs files (soon to be all Memory_FS)

---
+++ memory_fs/schemas/Schema__Cloud_FS__File.py
@@ -0,0 +1,10 @@
+from memory_fs.schemas.Schema__Cloud_FS__File__Info     import Schema__Cloud_FS__File__Info
+from osbot_utils.type_safe.Type_Safe                              import Type_Safe
+from memory_fs.schemas.Schema__Cloud_FS__File__Config   import Schema__Cloud_FS__File__Config
+from memory_fs.schemas.Schema__Cloud_FS__File__Metadata import Schema__Cloud_FS__File__Metadata
+
+
+class Schema__Cloud_FS__File(Type_Safe):
+    config   : Schema__Cloud_FS__File__Config
+    info     : Schema__Cloud_FS__File__Info
+    metadata : Schema__Cloud_FS__File__Metadata
