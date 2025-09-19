b21f12576c6ebbf714335594b66eb19301cc575b
Dinis Cruz <dinis.cruz@owasp.org>
2025-09-19 17:06:52 +0100

    fixed import

--- memory_fs/schemas/Schema__Memory_FS__File__Metadata.py
+++ memory_fs/schemas/Schema__Memory_FS__File__Metadata.py
@@ -1,6 +1,6 @@
 from typing                                                                         import Optional, Set, Dict, Any
 from osbot_utils.type_safe.primitives.domains.files.safe_uint.Safe_UInt__FileSize   import Safe_UInt__FileSize
-from osbot_utils.type_safe.primitives.domains.identifiers.Timestamp_Now             import Timestamp_Now
+from osbot_utils.type_safe.primitives.domains.identifiers.safe_int.Timestamp_Now             import Timestamp_Now
 from osbot_utils.type_safe.primitives.domains.identifiers.safe_str.Safe_Str__Id                   import Safe_Str__Id
 from osbot_utils.type_safe.primitives.domains.files.safe_str.Safe_Str__File__Path   import Safe_Str__File__Path
 from osbot_utils.type_safe.Type_Safe                                                import Type_Safe
